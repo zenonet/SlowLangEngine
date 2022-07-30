041f5af034ca115489e91ab09b63e2a4ce673211
zenonet <78077158+zenonet@users.noreply.github.com>
2022-07-30 16:05:37 +0200

    Created the StatementExtension class

---
+++ SlowLang.Engine/StatementExtension.cs
@@ -0,0 +1,17 @@
+﻿using SlowLang.Engine.Statements;
+using SlowLang.Engine.Tokens;
+
+namespace SlowLang.Engine;
+
+public abstract class StatementExtension : Statement
+{
+    protected override void OnParse(ref TokenList list)
+    {
+        
+    }
+
+    public virtual void OnParse(ref TokenList list, Statement baseStatement)
+    {
+        
+    }
+}
