fb72f203bf7a8303a794f925ecc8fa1f2db6c6c7
zenonet <78077158+zenonet@users.noreply.github.com>
2022-07-31 13:50:25 +0200

    Added an example project

---
+++ HelloWorldLang/HelloWorldStatement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SlowLang.Engine;
+using SlowLang.Engine.Statements;
+using SlowLang.Engine.Statements.StatementRegistrations;
+using SlowLang.Engine.Tokens;
+using SlowLang.Engine.Values;
+
+namespace HelloWorldLang;
+
+public class HelloWorldStatement : Statement
+{
+    public static void OnInitialize()
+    {
+        StatementRegistration.Create<HelloWorldStatement>(
+                TokenType.Keyword,
+                TokenType.OpeningBrace,
+                TokenType.String,
+                TokenType.ClosingBrace,
+                TokenType.Semicolon
+            )
+            .Register();
+    }
+
+    private string parameter = null!;
+    
+    protected override void OnParse(ref TokenList list)
+    {
+        string keyword = list.Pop().RawContent; // Cut the keyword
+        
+        //Check if the keyword really is helloworld
+        //If not, throw an error
+        if(keyword.ToLower() != "helloworld")
+            LoggingManager.LogError($"Unable to parse {keyword}", LineNumber);
+        
+        list.Pop(); // Cut the opening brace
+
+        //Get the token between the braces
+        parameter = list.Pop().RawContent;
+
+        //Because we didn't override CutTokensManually(), it defaults to false meaning we
+        //don't have to worry about completely removing the statement from the TokenList
+        //The parser will just remove the parsed TokenType sequence for us
+    }
+
+    public override Value Execute()
+    {
+        
+        Console.WriteLine($"Hello {parameter}");
+        
+        //The value to return if a Statement doesn't have a return value
+        return SlowVoid.I;
+    }
+}
---
+++ HelloWorldLang/Program.cs
@@ -0,0 +1,11 @@
+﻿using SlowLang.Engine;
+using SlowLang.Engine.Statements;
+using SlowLang.Engine.Tokens;
+
+const string path = @"..\..\..\first.helloWorldLang";
+
+string code = File.ReadAllText(path);
+TokenList tokenList = Lexer.Lex(code);
+
+Statement[] statements = Statement.ParseMultiple(tokenList);
+statements.Execute();
