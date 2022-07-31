212ed685780fbbc8fdcb7df71c8f962da2931a7c
zenonet <78077158+zenonet@users.noreply.github.com>
2022-07-31 13:39:40 +0200

    Added the Operator class

---
+++ SlowLang.Engine/Operator.cs
@@ -0,0 +1,23 @@
+﻿using SlowLang.Engine;
+using SlowLang.Engine.Statements;
+using SlowLang.Engine.Values;
+
+namespace SlowLang.Engine;
+
+public abstract class Operator : StatementExtension
+{
+    public Statement LeftOperand = null!;
+    public Statement RightOperand = null!;
+
+    protected Operator(Statement leftOperand, Statement rightOperand)
+    {
+        LeftOperand = leftOperand;
+        RightOperand = rightOperand;
+    }
+
+    protected Operator()
+    {
+        
+    }
+}
+public delegate Value OperatorDefinition(Value leftOperand, Value rightOperand);
