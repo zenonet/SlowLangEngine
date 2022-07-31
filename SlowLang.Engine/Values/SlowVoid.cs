590626aa8859935c35bcbfbea0c0d7d593401c29
zenonet <78077158+zenonet@users.noreply.github.com>
2022-07-31 19:34:17 +0200

    Added a value parser for SlowVoid

--- SlowLang.Engine/Values/SlowVoid.cs
+++ SlowLang.Engine/Values/SlowVoid.cs
@@ -1,4 +1,6 @@
-﻿namespace SlowLang.Engine.Values;
+﻿using SlowLang.Engine.Tokens;
+
+namespace SlowLang.Engine.Values;
 
 /// <summary>
 /// Used as the return type of void functions
@@ -6,4 +8,17 @@
 public class SlowVoid : Value
 {
     public static readonly SlowVoid I = new ();
+    
+    public static bool TryParse(ref TokenList tokenList, out Value? value)
+    {
+        if (tokenList.Peek().RawContent == "void")
+        {
+            tokenList.Pop();
+            value = I;
+            return true;
+        }
+
+        value = null;
+        return false;
+    }
 }
