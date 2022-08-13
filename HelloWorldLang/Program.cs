cd9fad505ea28e7cb869215104c71ed6b8832dc5
zenonet <78077158+zenonet@users.noreply.github.com>
2022-08-13 19:41:38 +0200

    Added a comment

--- HelloWorldLang/Program.cs
+++ HelloWorldLang/Program.cs
@@ -8,4 +8,7 @@ string code = File.ReadAllText(path);
 TokenList tokenList = Lexer.Lex(code);
 
 Statement[] statements = Statement.ParseMultiple(tokenList);
+
+//This is an extension method on IEnumerable<Statement>
+//that will execute all Statements in the list.
 statements.Execute();
