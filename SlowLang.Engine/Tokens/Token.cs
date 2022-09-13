40b45870d027b03785ce292f4a766e7661d04afd
zenonet <78077158+zenonet@users.noreply.github.com>
2022-09-13 20:22:16 +0200

    Created the Token.Clone() method

--- SlowLang.Engine/Tokens/Token.cs
+++ SlowLang.Engine/Tokens/Token.cs
@@ -14,4 +14,12 @@ public class Token
     }
 
     public override string ToString() => $"{Type}:{RawContent} in line {LineNumber}";
+
+    /// <summary>
+    /// Clones a Token without leaving any references to the original
+    /// </summary>
+    public Token Clone()
+    {
+        return new Token(RawContent, Type, LineNumber);
+    }
 }
