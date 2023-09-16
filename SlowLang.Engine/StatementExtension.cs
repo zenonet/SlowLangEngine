82bae936af7b1ac9e016e7fc5c11ead2301c7e8c
zenonet <78077158+zenonet@users.noreply.github.com>
2023-09-16 23:09:37 +0200

    Made StatementExtension.OnParse(list, baseStatement) return bool instead of void

--- SlowLang.Engine/StatementExtension.cs
+++ SlowLang.Engine/StatementExtension.cs
@@ -5,12 +5,13 @@ namespace SlowLang.Engine;
 
 public abstract class StatementExtension : Statement
 {
-    protected override bool OnParse(ref TokenList list)
+    public override bool OnParse(ref TokenList list)
     {
         return true;
     }
 
-    public virtual void OnParse(ref TokenList list, Statement baseStatement)
+    public virtual bool OnParse(ref TokenList list, Statement baseStatement)
     {
+        return true;
     }
 }
